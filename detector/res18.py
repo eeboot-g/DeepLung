6ec72201455df33690a9d6c9c20badf6e8807ed7
Minerva <54566780+Minerva-jiezhao@users.noreply.github.com>
2019-11-04 11:07:51 +0800

    Update res18.py

--- detector/res18.py
+++ detector/res18.py
@@ -58,7 +58,6 @@ class Net(nn.Module):
                     blocks.append(PostRes(self.featureNum_forw[i+1], self.featureNum_forw[i+1]))
             setattr(self, 'forw' + str(i + 1), nn.Sequential(*blocks))
 
-            
         for i in range(len(num_blocks_back)):
             blocks = []
             for j in range(num_blocks_back[i]):
@@ -94,36 +93,42 @@ class Net(nn.Module):
                                    nn.Conv3d(64, 5 * len(config['anchors']), kernel_size = 1))
 
     def forward(self, x, coord):
-        out = self.preBlock(x)#16
-        out_pool,indices0 = self.maxpool1(out)
-        out1 = self.forw1(out_pool)#32
-        out1_pool,indices1 = self.maxpool2(out1)
-        out2 = self.forw2(out1_pool)#64
-        #out2 = self.drop(out2)
-        out2_pool,indices2 = self.maxpool3(out2)
-        out3 = self.forw3(out2_pool)#96
-        out3_pool,indices3 = self.maxpool4(out3)
-        out4 = self.forw4(out3_pool)#96
-        #out4 = self.drop(out4)
+        # print('x, coord',x.shape, coord.shape)#torch.Size([BS, 1, 96, 96, 96]) torch.Size([BS, 3, 24, 24, 24])
+        out = self.preBlock(x)#torch.Size([BS, 24, 96, 96, 96])
         
-        rev3 = self.path1(out4)
-        comb3 = self.back3(torch.cat((rev3, out3), 1))#96+96
+        out_pool,indices0 = self.maxpool1(out)#torch.Size([BS, 24, 48, 48, 48])
+        out1 = self.forw1(out_pool)#torch.Size([BS, 32, 48, 48, 48])
+        out1_pool,indices1 = self.maxpool2(out1)#torch.Size([BS, 32, 24, 24, 24])
+        out2 = self.forw2(out1_pool)#torch.Size([BS, 64, 24, 24, 24])
+        #out2 = self.drop(out2)
+        out2_pool,indices2 = self.maxpool3(out2)#torch.Size([BS, 64, 12, 12, 12])
+        out3 = self.forw3(out2_pool)#torch.Size([BS, 64, 12, 12, 12])
+        out3_pool,indices3 = self.maxpool4(out3)#torch.Size([BS, 64, 6, 6, 6])
+        out4 = self.forw4(out3_pool)#torch.Size([BS, 64, 6, 6, 6])
+        #out4 = self.drop(out4)
+        # print('out',out_pool.shape,out1.shape,out1_pool.shape,out2.shape,out2_pool.shape,out3.shape,out3_pool.shape,out4.shape)
+        rev3 = self.path1(out4)#torch.Size([7, 64, 12, 12, 12])
+        comb3 = self.back3(torch.cat((rev3, out3), 1))#torch.Size([7, 64, 12, 12, 12])
         #comb3 = self.drop(comb3)
-        rev2 = self.path2(comb3)
+        rev2 = self.path2(comb3)#torch.Size([7, 64, 24, 24, 24])
         
         comb2 = self.back2(torch.cat((rev2, out2,coord), 1))#64+64
-        comb2 = self.drop(comb2)
-        out = self.output(comb2)
+        comb2 = self.drop(comb2)#torch.Size([7, 128, 24, 24, 24])
+        out = self.output(comb2)#torch.Size([7, 15, 24, 24, 24])
         size = out.size()
-        out = out.view(out.size(0), out.size(1), -1)
+        # print('out',out.shape)
+        out = out.view(out.size(0), out.size(1), -1)#torch.Size([7, 15, 13824])
+        # print('out',out.shape)
         #out = out.transpose(1, 4).transpose(1, 2).transpose(2, 3).contiguous()
         out = out.transpose(1, 2).contiguous().view(size[0], size[2], size[3], size[4], len(config['anchors']), 5)
         #out = out.view(-1, 5)
-        return out
+        
+        return out#torch.Size([7, 24, 24, 24, 3, 5])
 
     
 def get_model():
     net = Net()
+    # print('Net----res18!', net)
     loss = Loss(config['num_hard'])
     get_pbb = GetPBB(config)
     return config, net, loss, get_pbb
