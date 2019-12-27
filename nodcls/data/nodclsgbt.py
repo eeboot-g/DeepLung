a94fd96e5257db7807e0e0e1e404d9b520d705ae
Minerva <54566780+Minerva-jiezhao@users.noreply.github.com>
2019-12-27 13:49:38 +0800

    Update nodclsgbt.py

--- nodcls/data/nodclsgbt.py
+++ nodcls/data/nodclsgbt.py
@@ -1,10 +1,13 @@
 import numpy as np
 import pandas as pd 
-import mahotas
-from mahotas.features.lbp import lbp
-CROPSIZE = 17#24#30#36
-print CROPSIZE
-pdframe =  pd.read_csv('annotationdetclsconv_v3.csv', names=['seriesuid', 'coordX', 'coordY', 'coordZ', 'diameter_mm', 'malignant'])
+# import mahotas
+# from mahotas.features.lbp import lbp
+# CROPSIZE = 17#24#30#36
+CROPSIZE = 32
+print('CROPSIZE',CROPSIZE)
+# pdframe =  pd.read_csv('annotationdetclsconv_v3.csv', names=['seriesuid', 'coordX', 'coordY', 'coordZ', 'diameter_mm', 'malignant'])
+
+pdframe =  pd.read_csv('annotationdetclsconvfnl_v3_O.csv', names=['seriesuid', 'coordX', 'coordY', 'coordZ', 'diameter_mm', 'malignant'])
 srslst = pdframe['seriesuid'].tolist()[1:]
 crdxlst = pdframe['coordX'].tolist()[1:]
 crdylst = pdframe['coordY'].tolist()[1:]
@@ -14,50 +17,55 @@ mlglst = pdframe['malignant'].tolist()[1:]
 
 newlst = []
 import csv
-fid = open('annotationdetclsconvfnl_v3.csv', 'w')
-writer = csv.writer(fid)
-writer.writerow(['seriesuid', 'coordX', 'coordY', 'coordZ', 'diameter_mm', 'malignant'])
-for i in xrange(len(srslst)):
-	writer.writerow([srslst[i]+'-'+str(i), crdxlst[i], crdylst[i], crdzlst[i], dimlst[i], mlglst[i]])
+# fid = open('annotationdetclsconvfnl_v3.csv', 'w')
+# writer = csv.writer(fid)
+# writer.writerow(['seriesuid', 'coordX', 'coordY', 'coordZ', 'diameter_mm', 'malignant'])
+for i in range(len(srslst)):
+	# writer.writerow([srslst[i]+'-'+str(i), crdxlst[i], crdylst[i], crdzlst[i], dimlst[i], mlglst[i]])
 	newlst.append([srslst[i]+'-'+str(i), crdxlst[i], crdylst[i], crdzlst[i], dimlst[i], mlglst[i]])
-fid.close()
-
-preprocesspath = '/media/data1/wentao/tianchi/luna16/preprocess/lunaall/'
-savepath = '/media/data1/wentao/tianchi/luna16/cls/crop_v3/'
+# fid.close()
+print('newlst',len(newlst))
+preprocesspath = '/home/zhaojie/zhaojie/Lung/data/luna16/cls/train6-9/'
+savepath = '/home/zhaojie/zhaojie/Lung/data/luna16/cls/crop_v3/'
 import os
 import os.path 
-# if not os.path.exists(savepath): os.mkdir(savepath)
-# for idx in xrange(len(newlst)):
-# 	fname = newlst[idx][0]
-# 	# if fname != '1.3.6.1.4.1.14519.5.2.1.6279.6001.119209873306155771318545953948-581': continue
-# 	pid = fname.split('-')[0]
-# 	crdx = int(float(newlst[idx][1]))
-# 	crdy = int(float(newlst[idx][2]))
-# 	crdz = int(float(newlst[idx][3]))
-# 	dim = int(float(newlst[idx][4]))
-# 	data = np.load(os.path.join(preprocesspath, pid+'_clean.npy'))
-# 	bgx = max(0, crdx-CROPSIZE/2)
-# 	bgy = max(0, crdy-CROPSIZE/2)
-# 	bgz = max(0, crdz-CROPSIZE/2)
-# 	cropdata = np.ones((CROPSIZE, CROPSIZE, CROPSIZE))*170
-# 	cropdatatmp = np.array(data[0, bgx:bgx+CROPSIZE, bgy:bgy+CROPSIZE, bgz:bgz+CROPSIZE])
-# 	cropdata[CROPSIZE/2-cropdatatmp.shape[0]/2:CROPSIZE/2-cropdatatmp.shape[0]/2+cropdatatmp.shape[0], \
-# 	    CROPSIZE/2-cropdatatmp.shape[1]/2:CROPSIZE/2-cropdatatmp.shape[1]/2+cropdatatmp.shape[1], \
-# 	    CROPSIZE/2-cropdatatmp.shape[2]/2:CROPSIZE/2-cropdatatmp.shape[2]/2+cropdatatmp.shape[2]] = np.array(2-cropdatatmp)
-# 	assert cropdata.shape[0] == CROPSIZE and cropdata.shape[1] == CROPSIZE and cropdata.shape[2] == CROPSIZE
-# 	np.save(os.path.join(savepath, fname+'.npy'), cropdata)
-
+if not os.path.exists(savepath): os.mkdir(savepath)
+for idx in range(len(newlst)):
+	fname = newlst[idx][0]
+	# if fname != '1.3.6.1.4.1.14519.5.2.1.6279.6001.119209873306155771318545953948-581': continue
+	pid = fname.split('-')[0]
+	crdx = int(float(newlst[idx][1]))
+	crdy = int(float(newlst[idx][2]))
+	crdz = int(float(newlst[idx][3]))
+	dim = int(float(newlst[idx][4]))
+	# print(os.path.join(preprocesspath, pid+'_clean.npy'))
+	data = np.load(os.path.join(preprocesspath, pid+'_clean.npy'))
+	
+	bgx = max(0, crdx-CROPSIZE//2)
+	bgy = max(0, crdy-CROPSIZE//2)
+	bgz = max(0, crdz-CROPSIZE//2)
+	cropdata = np.ones((CROPSIZE, CROPSIZE, CROPSIZE))*170#(17, 17, 17)
+	cropdatatmp = np.array(data[0, bgx:bgx+CROPSIZE, bgy:bgy+CROPSIZE, bgz:bgz+CROPSIZE])#(17, 17, 17)
+	# print('cropdatatmp0.shape',np.min(cropdatatmp),np.max(cropdatatmp))
+	# print('cropdatatmp1.shape',np.min(cropdatatmp),np.max(cropdatatmp))
+	cropdata[CROPSIZE//2-cropdatatmp.shape[0]//2:CROPSIZE//2-cropdatatmp.shape[0]//2+cropdatatmp.shape[0], \
+	    CROPSIZE//2-cropdatatmp.shape[1]//2:CROPSIZE//2-cropdatatmp.shape[1]//2+cropdatatmp.shape[1], \
+	    CROPSIZE//2-cropdatatmp.shape[2]//2:CROPSIZE//2-cropdatatmp.shape[2]//2+cropdatatmp.shape[2]] = np.array(2-cropdatatmp)
+	# print('cropdata.shape',np.min(2-cropdatatmp),np.max(2-cropdatatmp))
+	assert cropdata.shape[0] == CROPSIZE and cropdata.shape[1] == CROPSIZE and cropdata.shape[2] == CROPSIZE
+	# np.save(os.path.join(savepath, fname+'.npy'), cropdata)
+print(stop)
 # train use gbt
-subset1path = '/media/data1/wentao/tianchi/luna16/subset1/'
+subset1path = '/home/zhaojie/zhaojie/Lung/data/luna16/subset_data/subset1/'
 testfnamelst = []
 for fname in os.listdir(subset1path):
 	if fname.endswith('.mhd'):
 		testfnamelst.append(fname[:-4])
 ntest = 0
-for idx in xrange(len(newlst)):
+for idx in range(len(newlst)):
 	fname = newlst[idx][0]
 	if fname.split('-')[0] in testfnamelst: ntest +=1
-print 'ntest', ntest, 'ntrain', len(newlst)-ntest
+print('ntest', ntest, 'ntrain', len(newlst)-ntest)
 
 traindata = np.zeros((len(newlst)-ntest, CROPSIZE*CROPSIZE*CROPSIZE))
 trainlabel = np.zeros((len(newlst)-ntest,))
@@ -65,14 +73,14 @@ testdata = np.zeros((ntest, CROPSIZE*CROPSIZE*CROPSIZE))
 testlabel = np.zeros((ntest,))
 
 trainidx = testidx = 0
-for idx in xrange(len(newlst)):
+for idx in range(len(newlst)):
 	fname = newlst[idx][0]
 	# print fname
 	data = np.load(os.path.join(savepath, fname+'.npy'))
 	# print data.shape
-	bgx = data.shape[0]/2-CROPSIZE/2
-	bgy = data.shape[1]/2-CROPSIZE/2
-	bgz = data.shape[2]/2-CROPSIZE/2
+	bgx = data.shape[0]//2-CROPSIZE//2
+	bgy = data.shape[1]//2-CROPSIZE//2
+	bgz = data.shape[2]//2-CROPSIZE//2
 	data = np.array(data[bgx:bgx+CROPSIZE, bgy:bgy+CROPSIZE, bgz:bgz+CROPSIZE])
 	if fname.split('-')[0] in testfnamelst:
 		testdata[testidx, :] = np.reshape(data, (-1,)) / 255
@@ -120,7 +128,17 @@ acclst = p.map(gbtfunc, range(1,9,1))#3,4,1))#5,1))#1,9,1))
 for acc in acclst:
 	print("{0:.4f}".format(acc[0]), "{0:.4f}".format(acc[1]))
 p.close()
-# for dep in xrange(1,9,1):
+# ntest 107 ntrain 897
+# trainacc  testacc
+# 0.8763 0.8037
+# 0.9554 0.8411
+# 0.9978 0.8505
+# 1.0000 0.8692
+# 1.0000 0.8411
+# 1.0000 0.8224
+# 1.0000 0.8131
+# 1.0000 0.8131
+# for dep in range(1,9,1):
 # 	m = gbt(max_depth=dep)
 # 	m.fit(traindata, trainlabel)
 # 	print dep, 'trainacc', np.sum(m.predict(traindata) == trainlabel) / float(traindata.shape[0])
