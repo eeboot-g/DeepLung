888654e7dc731bee31bc02247c0f9998dd1fced3
Minerva <54566780+Minerva-jiezhao@users.noreply.github.com>
2019-12-27 13:52:13 +0800

    Update kappatest.py

--- nodcls/data/kappatest.py
+++ nodcls/data/kappatest.py
@@ -6,8 +6,8 @@ pn = [0]*4#[12]*4#[0]*4
 npp = [0]*4#[47]*4#[0]*4
 
 for fd in [1,2,3,5]:#xrange(1,4,1):
-	dctprd = np.load('/media/data1/wentao/tianchi/luna16/CSVFILES/dctptlabel'+str(fd)+'.npy').item()
-	for d in xrange(4):
+	dctprd = np.load('/home/zhaojie/zhaojie/Lung/code/nodcls/data/npy/dctptlabel'+str(fd)+'.npy', allow_pickle = True).item()
+	for d in range(4):
 		modprd = np.load('modprd'+str(d+1)+'fd'+str(fd)+'.npy').item()
 		for k, v in dctprd.iteritems():
 			if v[d] != -1:
@@ -17,7 +17,7 @@ for fd in [1,2,3,5]:#xrange(1,4,1):
 				if v[d] == 1 and modprd[k] == 0: pn[d] += 1
 				if v[d] == 0 and modprd[k] == 1: npp[d] += 1
 print(pp, nn, pn, npp)
-for d in xrange(4):
+for d in range(4):
 	n = pp[d] + nn[d] + pn[d] + npp[d]
 	p0 = (pp[d] + nn[d]) / float(n)
 	pe = (pp[d] + pn[d]) * (pp[d] + npp[d])
